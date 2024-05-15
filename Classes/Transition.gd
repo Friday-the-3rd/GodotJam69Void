9bc2c8bb2b1e521f2af9b667ed0b8af7d53eda87
SzakalMaster <szakalmastergames@gmail.com>
2024-05-15 15:35:15 +0200

    Hospital Level Added new level and materials for it

--- Classes/Transition.gd
+++ Classes/Transition.gd
@@ -4,14 +4,13 @@ class_name Transition
 @export_file() var path_to_next_scene : String = "res://Levels/Level0.tscn"
 @export var locked := false
 
-@onready var AnimTree := $AnimationTree
+@onready var AnimPlayer := $AnimationPlayer
 
 signal door_opened
 
 func use():
-	AnimTree["parameters/conditions/is_opened"] = true
+	AnimPlayer.play("open")
 	
 
 func _on_animation_tree_animation_finished(_anim_name):
-	AnimTree["parameters/conditions/is_opened"] = false
 	door_opened.emit()
--- Scenes/item_inspect.gd
+++ Scenes/item_inspect.gd
@@ -6,7 +6,7 @@ extends CanvasLayer
 @onready var Camera := $SubViewportContainer/SubViewport/Camera3D
 
 var lmb_start_pos := Vector2.ZERO
-var cur_rotation := Vector3( deg_to_rad(15),0,deg_to_rad(30))
+var cur_rotation := Vector3( 0,0,0)
 
 const MOUSE_SENS := 0.002
 
@@ -14,8 +14,6 @@ func set_object(object : ItemObject3D):
 	Item.mesh = object.item_mesh()
 	ItemPan.get_node("Item Name").text = "[center][wave]"+object.item_name()+"[/wave][/center]"
 	ItemPan.get_node("Item Desc").text = "[center]"+object.item_desc()+"[/center]"
-	lmb_start_pos = Vector2.ZERO
-	cur_rotation = Vector3( deg_to_rad(15),0,deg_to_rad(30))
 
 func _ready():
 	Item.global_rotation = cur_rotation
@@ -36,7 +34,7 @@ func zoom():
 		Camera.global_position = lerp(Camera.global_position, Vector3(0,0,4),0.2)
 
 func rotate_inspect(event):
-	if Input.is_action_pressed("item_inspect"):			
+	if Input.is_action_pressed("item_inspect"):
 		if event is InputEventMouseMotion:
 			Item.rotate_y(-event.relative.x * 4 * MOUSE_SENS)
 			Item.rotate_x(-event.relative.y * 4 * MOUSE_SENS)
