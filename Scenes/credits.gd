1dbaacf13bebf78438ac5a030ed6b9e476c9c48c
SzakalMaster <szakalmastergames@gmail.com>
2024-05-19 21:58:29 +0200

    x

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -1,9 +1,11 @@
 extends PanelContainer
 
+var mmmusic = preload("res://Sounds/strings.mp3")
 
 func _ready():
 	TransitionScreen.get_node("AnimationPlayer").play("FadeOut")
-
+	Music.stream = mmmusic
+	Music.play()
 
 func _on_back_pressed():
 	TransitionScreen.change_to_new_scene("res://Scenes/credits.tscn")
